96738bd9d3e27fc0d77ff52e878efc48c178fbff
Kludák Dániel <qwerty@qwerty.local>
2025-04-22 14:09:43 +0200

    Add backend folder

---
+++ vizsgaremek-backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Test1234!',
+      database: 'vizsgaremek',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
