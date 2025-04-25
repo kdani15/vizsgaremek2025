1d469f126b63e1df31744b40dfe4f28b3899ac51
stevespectre <istvan.erdos86@gmail.com>
2025-04-25 12:25:50 +0200

    Fix cors error for production

--- vizsgaremek-backend/src/main.ts
+++ vizsgaremek-backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND_APP_URL],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
